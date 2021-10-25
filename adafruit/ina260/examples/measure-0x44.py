297dc3d298d1fbf9a4399955d43d2ade3b46f540
Andre Jacobs <andrejacobs@users.noreply.github.com>
2021-10-25 16:21:51 +0100

    Added two copies of the measure script to read from 0x41 and 0x44 I2C devices

---
+++ adafruit/ina260/examples/measure-0x44.py
@@ -0,0 +1,16 @@
+# Source from: https://learn.adafruit.com/adafruit-ina260-current-voltage-power-sensor-breakout/python-circuitpython
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import time
+import board
+import adafruit_ina260
+
+i2c = board.I2C()
+ina260 = adafruit_ina260.INA260(i2c, address=0x44)
+while True:
+    print(
+        "Current: %.2f mA Voltage: %.2f V Power:%.2f mW"
+        % (ina260.current, ina260.voltage, ina260.power)
+    )
+    time.sleep(1)
